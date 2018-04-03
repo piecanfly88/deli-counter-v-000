9311ec1b74491215de829fe393e5175ad7e22b29
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-03 23:37:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(katz_deli)
+  current_line = "The line is currently empty."
+  katz_deli.each_index do |i|
+    position = i + 1
+    person = katz_deli[i]
+    if i == 0
+      current_line = "The line is currently: #{position}. #{person}"
+    else
+      current_line += " #{position}. #{person}"
+    end
+  end
+  current_line
+end
